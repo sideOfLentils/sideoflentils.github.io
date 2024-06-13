6fdf9c6ef2317db17b18b2f54c1d23afd695e7ea
David Condroski <dcondroski@gmail.com>
2024-06-13 10:30:41 -0500

    Updates

---
+++ _app/immutable/nodes/2.Nl-JIDgr.js
@@ -0,0 +1,5 @@
+import{s as p,n as o}from"../chunks/scheduler.k-kUyWhY.js";import{S as h,i as f,g as l,s as v,z as _,h as d,f as i,c as x,x as b,k as c,y as M,a as m}from"../chunks/index.Gr9dGhNC.js";function y(u){let e,n,t,r=`<div class="business-card"><div class="business-card-text">David Condroski
+            <br/> <br/>
+            Master of Computer Science
+            <br/>
+            Student @ <span class="illinois">Illinois</span></div></div>`;return{c(){e=l("meta"),n=v(),t=l("div"),t.innerHTML=r,this.h()},l(s){const a=_("svelte-1qxfzmt",document.head);e=d(a,"META",{name:!0,content:!0}),a.forEach(i),n=x(s),t=d(s,"DIV",{class:!0,"data-svelte-h":!0}),b(t)!=="svelte-1ns1efc"&&(t.innerHTML=r),this.h()},h(){document.title="Home",c(e,"name","description"),c(e,"content","My Portfolio Website"),c(t,"class","container card")},m(s,a){M(document.head,e),m(s,n,a),m(s,t,a)},p:o,i:o,o,d(s){s&&(i(n),i(t)),i(e)}}}class g extends h{constructor(e){super(),f(this,e,null,y,p,{})}}export{g as component};
