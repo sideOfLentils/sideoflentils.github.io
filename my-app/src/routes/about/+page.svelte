b1742a58dabf489992e709a67b47e52ca29c7809
David Condroski <dcondroski@gmail.com>
2024-06-21 12:08:21 -0500

    updated the about page along with custom styling for text

--- my-app/src/routes/about/+page.svelte
+++ my-app/src/routes/about/+page.svelte
@@ -1,1 +1,20 @@
-<h1>About Me</h1>
+
+<section>
+    <h1>About Me</h1>
+    <p>
+        My time at the University of Illinois Urbana-Champaign has shown me what is possible throughout the field of computer science 
+        and how it connects to all other fields in ways I couldn't have imagined before coming to the university. 
+        Now that I am working on completing my Masters, I have a better sense of what is possible and what I can contribute, whether 
+        it be working in data science using SQL queries, 
+        python to construct a neural network using PyTorch, C++ to build a game with Unreal Engine 5, Haskell to write an interpreter for other programming languages, 
+        or C to communicate with other systems over a network using OpenMP and MPI, the possibilities are endless. 
+    </p>
+    <p>
+        Right now, I'm learning about what it takes to become an AI engineer 
+        while still expanding my knowledge of front-end web development with the creation of this portfolio website. 
+    </p>
+    <p>
+        In the future, I plan to explore how mobile applications are created to better understand the field of IoT within computer science. 
+    </p>
+</section>
+<p></p>
