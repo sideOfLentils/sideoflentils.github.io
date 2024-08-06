3ce4e91a4d07a13d82ce463ea31dc99984d757e8
sideOfLentils <69644044+sideOfLentils@users.noreply.github.com>
2024-08-06 03:11:23 -0500

    added meta tags for fixed positioning on mobile

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <svelte:head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Gothic+A1&display=swap" rel="stylesheet">
