3488b4237548b67946b398e67d964ab43a227113
sideOfLentils <dcondroski@gmail.com>
2025-11-03 01:17:48 -0600

    Updates

---
+++ _app/immutable/nodes/2.VN5TiTNF.js
@@ -0,0 +1,5 @@
+import{s as u,n as o}from"../chunks/CZmuBx9L.js";import{S as h,i as f,d as i,a as v,b as l,c as r,h as _,e as d,f as b,g as x,j as m,s as M}from"../chunks/2Eu0VRyT.js";function C(p){let e,n,t,c=`<div class="business-card"><div class="business-card-text">David Condroski
+            <br/> <br/>
+            Master of
+            <br/>
+            Computer Science @ <span class="illinois">Illinois</span></div></div>`;return{c(){e=m("meta"),n=M(),t=m("div"),t.innerHTML=c,this.h()},l(s){const a=_("svelte-1qxfzmt",document.head);e=d(a,"META",{name:!0,content:!0}),a.forEach(i),n=b(s),t=d(s,"DIV",{class:!0,"data-svelte-h":!0}),x(t)!=="svelte-1or9ldp"&&(t.innerHTML=c),this.h()},h(){document.title="Home",r(e,"name","description"),r(e,"content","My Portfolio Website"),r(t,"class","container card")},m(s,a){v(document.head,e),l(s,n,a),l(s,t,a)},p:o,i:o,o,d(s){s&&(i(n),i(t)),i(e)}}}class H extends h{constructor(e){super(),f(this,e,null,C,u,{})}}export{H as component};
